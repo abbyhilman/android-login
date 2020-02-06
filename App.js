a7c2f9ecaef947eea2bd101bf63255e374227c94
Abby Hilman <abbyhilman@gmail.com>
2020-02-06 20:03:53 +0700

    UPDATE

--- App.js
+++ App.js
@@ -1,53 +1,58 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: null };
+  //state = { loggedIn: null };
 
   componentDidMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDV3ezfupV-8JTp1dYst0uUr2rLa3hyVPc",
-      authDomain: "login-17bf8.firebaseapp.com",
-      databaseURL: "https://login-17bf8.firebaseio.com",
-      projectId: "login-17bf8",
-      storageBucket: "login-17bf8.appspot.com",
-      messagingSenderId: "240254838381",
-      appId: "1:240254838381:web:59680657b29a28f257c164",
-      measurementId: "G-Q0ZDZ18N6T"
+      apiKey: "your api key",
+      authDomain: "your auth domain",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
     });
 
-    firebase.auth().onAuthStateChanged((user) => {
+    /*firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({ loggedIn: true });
       } else {
         this.setState({ loggedIn: false });
       }
-    });
+    });*/
   }
 
-  renderContent() {
+  /*renderContent() {
     switch (this.state.loggedIn) {
       case true:
         return (
-          <Button onPress={() => firebase.auth().signOut()}>
-            Log Out
-          </Button>
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+
         );
       case false:
         return <LoginForm />;
       default:
         return <Spinner size="large" />;
     }
-  }
+  }{this.renderContent()}*/
 
   render() {
     return (
       <View>
-        <Header headerText="Authentication" />
-        {this.renderContent()}
+        <Header headerText="Login" />
+        <LoginForm />
       </View>
     );
   }
